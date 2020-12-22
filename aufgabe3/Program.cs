1f46708f0189f971e92ec44b5eed1d43c69be9a4
Lei Wei <idea.lew@gmail.com>
2020-12-22 18:17:56 +0100

    uebung 3

---
+++ aufgabe3/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace aufgabe3 {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+
+    
+}
+
+
+// 1
+//for(; ; ) {
+//                try {
+//                    Console.WriteLine("give the first number, like 1 or 1.3");
+//                    double a = double.Parse(Console.ReadLine().Trim());
+//Console.WriteLine("give the operator, it should be within: + - * / %");
+//                    string op = Console.ReadLine().Trim();
+//Console.WriteLine("give the second number, like 1 or 1.3");
+//                    double b = double.Parse(Console.ReadLine().Trim());
+//double res;
+//                    switch (op) {
+//                        case "+":
+//                            res = a + b;
+//                            break;
+//                        case "-":
+//                            res = a - b;
+//                            break;
+//                        case "*":
+//                            res = a* b;
+//                            break;
+//                        case "/":
+//                            res = a / b;
+//                            break;
+//                        default:
+//                            throw new Exception("operator not in range!");
+//                    }
+//                    Console.WriteLine($"{a} {op} {b} = {res} \n");
+//                } catch (Exception e) {
+//                    Console.WriteLine(e.Message + '\n');
+//                }
+//            }
+
+
+// 2
+//public class Human {
+//    public string Name { get; set; }
+//    private DateTime birthday;
+//    public DateTime Birthday {
+//        set {
+//            if (value.Year > 1900) birthday = value;
+//            else Console.WriteLine("too old!");
+//        }
+//    }
+//    public int Age {
+//        get {
+//            return DateTime.Now.Year - birthday.Year;
+//        }
+//    }
+
+//    public string Description {
+//        get {
+//            return $"{Name} is {Age} year(s) old";
+//        }
+//    }
+//}
+
+// 3
+//static async Task Main(string[] args) {
+//    int interval = 3 * 1000;
+//    for (; ; ) {
+//        await Task.Delay(interval);
+//        Console.WriteLine("drink water!");
+//        Console.Beep();
+//    }
+//}
---
+++ uebung3/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace uebung3 {
+    class Program {
+        static void Main(string[] args) {
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//// 1 try - catch - finally und throw
+//            try {
+//                 etwas buggy here
+//                int a = 2;
+//int b = 0;
+//Console.WriteLine(a / b);
+//            }catch(Exception e) {
+//                 error handling here
+//                Console.WriteLine(e);
+//            } finally {
+//                Console.WriteLine("finally always works");
+//            }
+
+// 2 Property and field
+//class Test {
+//    ////property eigenschaft
+//    //public string Name { get; set; }
+//    ////field feld
+//    //public int age;
+
+//    private string name;
+//    public string Name {
+//        get {
+//            Console.WriteLine(".....");
+//            return name;
+//        }
+//        set {
+//            if (value == "jack") {
+//                name = value;
+//            } else {
+//                Console.WriteLine("name is not jack, operation rejected");
+//            }
+
+//        }
+//    }
+//}
+
+
+//3 async Method
+//static async Task Main(string[] args) {
+//    HugeMethod();
+//    Console.WriteLine("finish");
+//}
+
+//static async Task HugeMethod() {
+//    for (int i = 0; i < 10; i++) {
+//        await Task.Delay(1000);
+//        Console.WriteLine(i);
+//    }
+//}
